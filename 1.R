0de238aef87b0d9933f9ed8410266f5e2985579b
Alxej <alexey.merkushev.2003@gmail.com>
2025-03-21 13:34:21 +0300

    4 lab edited

--- 1.R
+++ 1.R
@@ -6,15 +6,15 @@ salary <- c(90780.00, 685050.00, 54770.33, 230300.92, 31070.35)
 data <- data.frame(cityname, people, dist, salary)
 data
 
-setwd("C:/Users/Public/")
+setwd("C:/Users/Alxej/Documents")
 tab <- read.table("Cities.txt", header=TRUE)
 tab$people
 plot(x=tab$dist, y=tab$salary, xlab="Расстояние до Москвы", ylab="Средняя заработная плата", main="Диаграмма рассеяния", col="red", pch=2)
 text(x=tab$dist, y=tab$salary-10, labels=tab$cityname) # вывод подписей к точкам
 
-newyork_data <- data[data$cityname == "New-York",]
+newyork_data <- tab[tab$cityname == "New-York",]
 newyork_data
-penza_data <- data[data$cityname == "Penza",]
+penza_data <- tab[tab$cityname == "Penza",]
 d <- (newyork_data$salary - penza_data$salary) / (newyork_data$dist - penza_data$dist)
 d
 c <- penza_data$salary - d * penza_data$dist
--- 4.R
+++ 4.R
@@ -1,6 +1,6 @@
-setwd("D:/r labs/data/")
+setwd("C:/xd/r_labs/data/")
 T<-read.table("reg.txt", header=TRUE)
-second_var = "A12"
+second_var <- "A7"
 T<-T[order(T$A1),]
 reg9<-lm(formula=T[[second_var]] ~T$A1)
 
@@ -17,18 +17,18 @@ library("forecast")
 accuracy(reg9.1)
 coef(reg9.1)[1] + coef(reg9.1)[2]*log(1350)
 
-library(nls2)
+
 nlc <- nls.control(maxiter = 200)
-reg9.2<-nls(T[[second_var]]~p1 + p2/(p3 + T$A1), control = nlc, data=T, start=list(p1=500, p2=-1000, p3=-1000))
+reg9.2<-nls(T[[second_var]]~p2 *(p1 + T$A1) * (p1 + T$A1) + p3, control = nlc, data=T, start=list(p1=-500, p2=-500, p3=100))
 summary(reg9.2)
 P<-coef(reg9.2)
 plot(x=T$A1,y=T[[second_var]])
 abline(reg9, col="red")
 curve(coef(reg9.1)[1] + coef(reg9.1)[2]*log(x), add=TRUE, col="green")
-curve(P[1] + P[2]/(x+P[3]), add=TRUE, col="blue") 
+curve(P[2] * (P[1] + x) * (P[1] + x)+ P[3], add=TRUE, col="blue") 
 plot(reg9.2$m$resid(), main="График остатков", xlab="Номер наблюдения", ylab="Остатки")
 abline(h=0, col="red")
-sum(abs((T[[second_var]] - (P[1]+ P[2]/(P[3]+T$A1)))/T[[second_var]]))/length(T[[second_var]])*100
-P[1] + P[2]/(P[3] + 1350)
+sum(abs((T[[second_var]] - (P[2] * (P[1] + T$A1) * (P[1] + T$A1)+ P[3]))/T[[second_var]]))/length(T[[second_var]])*100
+P[2] * (P[1] + 1350) * (P[1] + 1350)+ P[3]
 P
 
